0491098d772f57fa459da08795b20e5ae0750d4a
Bencraft8 <jbenbonus@gmail.com>
2020-11-17 20:43:53 -0800

    added !odds to !help made a 1% chance for !coinflip to land on its side

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -2,7 +2,22 @@ module.exports = {
     name: 'coinflip',
     description: "this is the coinflip command!",
     run: async (message, args, client) => {
+        var num = Math.floor(Math.random() * 101) + 1;
+        var coin = "";
+        if(num < 50) {
+            coin = "Heads";
+        }
+        else if(num < 100) {
+            coin = "Tails";
+        }
+        else if(num == 101) {
+            coin = "oh my god! It landed on it's side";
+        }
+        else {
+            coin = "Heads";
+        }
+
         var coin = (Math.floor(Math.random() * 2) == 1) ? "Heads" : "Tails"
-        message.channel.send("The coin lands on... " + coin + "!")
+        message.channel.send(`The coin lands on... ${coin}!`)
     }
 }
--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const commandPrefixes = ["!", "."];
 const foodCommands = ["!pie, !pierate", "!muffin", "!potato, !tater", "!icecream", "!pizza", "!fish", "!menu (food)" ];
-const miscCommands = ["!help, !commands", "!stats, !info", "!global", "!server, !guild", "!ask (question)", "!scale (max) (question)", "!coinflip, !cf", "!ping" ];
+const miscCommands = ["!help, !commands", "!stats, !info", "!global", "!server, !guild", "!ask (question)", "!scale (max) (question)", "!odds, !chances", "!coinflip, !cf", "!ping" ];
 
 module.exports = {
     name: 'help',
