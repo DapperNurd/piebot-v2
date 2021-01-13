5421ca5bebc9c8045a36d81575759caeb49c7d54
Bencraft8 <jbenbonus@gmail.com>
2021-01-12 17:02:43 -0800

    fix stuff

--- commands/thonktext.js
+++ commands/thonktext.js
@@ -1,8 +1,8 @@
 require('events').EventEmitter.defaultMaxListeners = 20;
 
 module.exports = {
-    name: 'woweetext',
-    description: 'wowee text',
+    name: 'thonktext',
+    description: 'thonk text',
     run: async (message, args, client) => {
         message.react("798709011376635954");
     }
