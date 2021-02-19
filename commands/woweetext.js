2386601c0f8a8976fd0d680fa511a7480dd79e07
Bencraft8 <jbenbonus@gmail.com>
2021-02-18 23:02:09 -0800

    attempt 1 at making it not react to wowee when all that is said is the emoji

--- commands/woweetext.js
+++ commands/woweetext.js
@@ -4,6 +4,8 @@ module.exports = {
     name: 'woweetext',
     description: 'wowee text',
     run: async (message, args, client) => {
-        message.react("758396947769196575");
+        if(!message.content.starsWith("<:")) {
+            message.react("758396947769196575");
+        }
     }
 }
