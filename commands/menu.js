d4196af8389c27bc226e21d251e93d93546400d6
Bencraft8 <jbenbonus@gmail.com>
2021-04-01 00:34:25 -0700

    Examples => Example in menu

--- commands/menu.js
+++ commands/menu.js
@@ -13,7 +13,7 @@ module.exports = {
     description: "this is a menu command!",
     run: async (message, args, client) => {
         //if(args.length < 1) message.channel.send("Please specify which menu to view. \n**Examples**\n\`!menu pie\`\n\`!menu all\`").then(function (sentMessage) {
-        if(args.length < 1) message.channel.send("Please specify which menu to view. \n**Examples**\n\`!menu all\`").then(function (sentMessage) {
+        if(args.length < 1) message.channel.send("Please specify which menu to view. \n**Example**\n\`!menu all\`").then(function (sentMessage) {
             //sentMessage.react("🥧");
             sentMessage.react("827060748983074856");
             sentMessage.react("🥔");
