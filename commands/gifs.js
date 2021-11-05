4fb5e2d4f6a4675a552987c708abb45bbae244ba
DapperNurd <jbenbonus@gmail.com>
2021-11-05 11:28:23 -0700

    separated titties and gif command

--- commands/gifs.js
+++ commands/gifs.js
@@ -1,12 +1,12 @@
 const fetch = require('node-fetch');
 
 module.exports = {
-    name: 'tiddies',
+    name: 'gifs',
     description: "this is the ask command!",
     run: async (message, args, client) => {
         
         if(args.length > 0) {
-            var keywords = "anime tiddies";
+            var keywords = args.join(" ");
 
             var url = `https://g.tenor.com/v1/search?q=${keywords}&key=LIVDSRZULELA`
             let response = await fetch(url);
