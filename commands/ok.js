21ecd0c2ebae55b98fe16baf300bf1410b855221
DapperNurd <jbenbonus@gmail.com>
2021-11-08 18:39:35 -0800

    increased minimum on ok added 10% chance to capitalize ok

--- commands/ok.js
+++ commands/ok.js
@@ -4,7 +4,8 @@ module.exports = {
     name: 'ok',
     description: 'ok',
     run: async (message, args, client) => {
-        var random = Math.floor(Math.random() * (17 - 4)) + 4;
+        var random = Math.floor(Math.random() * (17 - 7)) + 7; // it's weird but basically this makes it so piebot will send a message after a random number of messages that aren't piebot
+                                                               // from 7 to 17, though I'm not sure how much the 17 max actually affects it
         message.channel.messages.fetch({ limit: random }).then(messages => {
             var authorIDs = [];
             for (let value of messages.values()) {
@@ -16,7 +17,12 @@ module.exports = {
             });
 
             if (!botID) {
-                message.channel.send("ok");
+                if(Math.ceil(Math.random() * 10) == 1) { // generates random number from 1 to 10
+                    message.channel.send("Ok");
+                } else {
+                    message.channel.send("ok");
+                }
+                
             }
 
         });
