2df0896674109263c6a3f185a0e72340f3d93263
DapperNurd <jbenbonus@gmail.com>
2021-12-01 20:15:10 -0800

    added roll to help command

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const mongoose = require('mongoose');
 
 const foodCommands = ["!pie, !pierate", "!muffin", "!potato, !tater", "!icecream", "!pizza", "!pasta", "!cake", "!sandwich", "!cookie", "!fish", "!menu (food)" ];
-const miscCommands = ["!help, !commands", "!stats, !info", "!global", "!server, !guild", "!ask (question)", "!gif (search term)","!scale (max) (question)", "!odds, !chances (question)", "!coinflip, !cf", "!ping" ];
+const miscCommands = ["!help, !commands", "!stats, !info", "!global", "!server, !guild", "!ask (question)", "!gif (search term)","!scale (max) (question)", "!odds, !chances (question)", "!coinflip, !cf", "!roll (max)", "!ping" ];
 
 module.exports = {
     name: 'help',
