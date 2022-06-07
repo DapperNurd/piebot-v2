b8bfc9f904354d0f09595f15dd331f93ecc56884
DapperNurd <jbenbonus@gmail.com>
2022-06-06 21:02:24 -0700

    uses new emojis on think and thonk command

--- commands/thonk.js
+++ commands/thonk.js
@@ -6,7 +6,7 @@ module.exports = {
         if(randomNum < 5) {
             message.react("😂");
         } else {
-            const emoji = (message.content.split(" ")[0].toLowerCase().substring(1) == "thonk") ? "<:greenThonkNurd:798709011376635954>" : "<:greenThinkNurd:798709011364577330> "
+            const emoji = (message.content.split(" ")[0].toLowerCase().substring(1) == "thonk") ? "<:nurdThonk:983576552488984586>" : "<:nurdThink:983576550811263036>"
             message.channel.send(emoji);
             if(message.guild.me.hasPermission("MANAGE_MESSAGES")) {
                 message.delete();
