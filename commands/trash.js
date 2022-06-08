f1f52f25f3f2b7be6fa416cfc949a2f5460d9d7e
DapperNurd <jbenbonus@gmail.com>
2022-06-07 23:27:22 -0700

    more work on trash command

--- commands/trash.js
+++ commands/trash.js
@@ -3,22 +3,65 @@ const Guild = require('../models/guild');
 const User = require('../models/user');
 const GlobalCount = require('../models/globalCount');
 
-const commonTrash = ["banana nut muffin", "blueberry muffin", "lemon poppy seed muffin", "coconut muffin", "oatmeal muffin", "raspberry muffin"
-];
+// 14 categories
 
-const uncommonTrash = ["chocolate chip muffin", "cornbread muffin", "pumpkin muffin", "coffee cake muffin", "peanut butter muffin", "maple walnut muffin", "pecan muffin"
-];
+const food = [];
+const foodAdj = [];
 
-const rareTrash = ["chocolate chunk muffin", "apple cinnamon muffin", "snickerdoodle muffin"
-];
+const drinks = [];
+const drinksAdj = [];
 
-const legendaryTrash = [`Kecatas' "special" muffin`, "drudanae muffin", "muffin time"];
+const clothing = [];
+const clothingAdj = [];
 
+const electronics = [];
+const electronicsAdj = [];
+
+const toys = [];
+const toysAdj = [];
+
+const gross = [];
+const grossAdj = [];
+
+const toiletries = [];
+const toiletriesAdj = [];
+
+const illegal = [];
+const illegalAdj = [];
+
+const tools = [];
+const toolsAdj = [];
+
+const kitchen = [];
+const kitchenAdj = [];
+
+const collectibles = [];
+const collectiblesAdj = [];
+
+const animals = [];
+const animalsAdj = [];
+
+const furniture = [];
+const furnitureAdj = [];
+
+const misc = [];
+const miscAdj = [];
+
+// only here for viewing purposes, will delete when done
 const trashPhrases = ["Here, [USER]! Kecatas wants you to have one of his [MUFFIN ADJ] [MUFFIN]s!",
     "[USER], you have stumbled upon Kecatas's stash of [MUFFIN ADJ] [MUFFIN]s. He won't know if you take just one, right?",
     "From the kitchen you can smell that Kecatas has prepared a batch of [MUFFIN ADJ] [MUFFIN]s. He offers [USER] one. What a good guy!",
     "Kecatas spent all morning baking a dozen [MUFFIN ADJ] [MUFFIN]s and wants you to try one, [USER]!",
     "Muffin Master Kecatas is testing a new recipe of [MUFFIN ADJ] [MUFFIN]s and gives you one to try. How does it taste, [USER]?"];
+
+// list of phrases that piebot can pick from when sending a message
+const phrases = [ "[USER] goes rummaging through the garbage and finds [ITEM]. They have found [COUNT] things in the trash.",
+                  "[USER] dug through Trash's special dumpster and found [item]. they have found [COUNT] pieces of trash. you gonna pay for that?",
+                  "[USER] dove into the dumpster for the [COUNT] time and found [ITEM].",
+                  "[USER] accidentally knocked over a trash can and [ITEM] fell out. They have found [COUNT] things in the trash.",
+                  "Trashed has graced [USER] with [ITEM] from his personal bin. They have gathered [COUNT] pieces of trash." ];
+
+const specialPhrase = "[USER] went dumpster diving and found [ITEM]! how did they even find it? they have found [COUNT] items collecting garbage. He wont be happy when he finds out.";
 
 module.exports = {
     name: 'trash',
@@ -85,9 +128,6 @@ module.exports = {
             }
         });
 
-        var userTrashCount = userUniqueCounts.trashCount;
-        userTrashCount++;
-
         const globalVar = await GlobalCount.findOne({
             globalID: "global"
         }, (err, guild) => {
@@ -117,51 +157,104 @@ module.exports = {
             }
         });
 
+        // taking the user's item count from the database and adding one to it locally
+        var userTrashCount = userUniqueCounts.trashCount;
+        userTrashCount++;
+
+        // taking the servers's item count from the database and adding one to it locally
         var trashCountNum = trashCountVar.trashCount;
         trashCountNum++;
 
+        // taking the global item count from the database and adding one to it locally
         var globalTrashCount = globalVar.trashCount;
         globalTrashCount++;
 
+        // sets person response to either the user or the first argument, depending on the existance of the argument
         var trashPerson = (args.length > 0) ? args[0] : message.author;
 
-        var randomNum = Math.floor(Math.random() * 100) + 1;
-        switch (true) {
-            case (randomNum < 50):
-                newTrash = commonTrash[Math.floor(Math.random() * commonTrash.length)];
+        var newTrash;
+        var trashAdj;
+
+
+        // FIGURE OUT CHANCE TO HAVE NO ADJECTIVE
+
+
+        // generates a number from 1 to 14 to pick one of the 14 categories
+        var randomCategory = Math.floor(Math.random() * 14) + 1;
+        switch (randomCategory) {
+            case (1):
+                newTrash = food[Math.floor(Math.random() * food.length)];
+                trashAdj = foodAdj[Math.floor(Math.random() * foodAdj.length)];
                 break;
-            case (randomNum < 95):
-                newTrash = uncommonTrash[Math.floor(Math.random() * uncommonTrash.length)];
+            case (2):
+                newTrash = drink[Math.floor(Math.random() * drink.length)];
+                trashAdj = drinkAdj[Math.floor(Math.random() * drinkAdj.length)];
                 break;
-            case (randomNum < 100):
-                newTrash = rareTrash[Math.floor(Math.random() * rareTrash.length)];
+            case (3):
+                newTrash = clothing[Math.floor(Math.random() * clothing.length)];
+                trashAdj = clothingAdj[Math.floor(Math.random() * clothingAdj.length)];
                 break;
-            case (randomNum >= 100):
-                newTrash = legendaryTrash[Math.floor(Math.random() * legendaryTrash.length)];
+            case (4):
+                newTrash = electronics[Math.floor(Math.random() * electronics.length)];
+                trashAdj = electronicsAdj[Math.floor(Math.random() * electronicsAdj.length)];
+                break;
+            case (5):
+                newTrash = toys[Math.floor(Math.random() * toys.length)];
+                trashAdj = toysAdj[Math.floor(Math.random() * toysAdj.length)];
+                break;
+            case (6):
+                newTrash = gross[Math.floor(Math.random() * gross.length)];
+                trashAdj = grossAdj[Math.floor(Math.random() * grossAdj.length)];
+                break;
+            case (7):
+                newTrash = toiletries[Math.floor(Math.random() * toiletries.length)];
+                trashAdj = toiletriesAdj[Math.floor(Math.random() * toiletriesAdj.length)];
+                break;
+            case (8):
+                newTrash = illegal[Math.floor(Math.random() * illegal.length)];
+                trashAdj = illegalAdj[Math.floor(Math.random() * illegalAdj.length)];
+                break;
+            case (9):
+                newTrash = tools[Math.floor(Math.random() * tools.length)];
+                trashAdj = toolsAdj[Math.floor(Math.random() * toolsAdj.length)];
+                break;
+            case (10):
+                newTrash = kitchen[Math.floor(Math.random() * kitchen.length)];
+                trashAdj = kitchenAdj[Math.floor(Math.random() * kitchenAdj.length)];
+                break;
+            case (11):
+                newTrash = collectibles[Math.floor(Math.random() * collectibles.length)];
+                trashAdj = collectiblesAdj[Math.floor(Math.random() * collectiblesAdj.length)];
+                break;
+            case (12):
+                newTrash = animals[Math.floor(Math.random() * animals.length)];
+                trashAdj = animalsAdj[Math.floor(Math.random() * animalsAdj.length)];
+                break;
+            case (13):
+                newTrash = furniture[Math.floor(Math.random() * furniture.length)];
+                trashAdj = furnitureAdj[Math.floor(Math.random() * furniture.Adjlength)];
+                break;
+            case (14):
+                newTrash = misc[Math.floor(Math.random() * misc.length)];
+                // misc is adjective-less
+                trashAdj = "";
                 break;
             default:
-                newTrash = commonTrash[Math.floor(Math.random() * commonTrash.length)];
+                // using toys as the default because it has the most items, but it should never actually be called this way with the way the random generator works
+                newTrash = toys[Math.floor(Math.random() * toys.length)];
+                trashAdj = toys[Math.floor(Math.random() * toys.length)];
         }
 
-        var adjRandom = Math.floor(Math.random() * 100) + 1;
-        var trashAdj = (adjRandom > 10) ? adjectives[Math.floor(Math.random() * adjectives.length)] : negAdjectives[Math.floor(Math.random() * negAdjectives.length)];
+        // sets the phrase to either a random phrase or the special phrase if the trash gotten was the secret stash
+        var phrase = (newTrash == "Trash's secret stash") ? specialPhrase : trashPhrases[Math.floor(Math.random() * trashPhrases.length)];
 
-        var phrase = trashPhrases[Math.floor(Math.random() * trashPhrases.length)];
+        // replaces all the placeholders in the phrase with the proper information
         phrase = phrase.replace('[USER]', trashPerson);
-        phrase = phrase.replace('[MUFFIN ADJ]', trashAdj);
-        phrase = phrase.replace('[MUFFIN]', newTrash);
+        phrase = phrase.replace('[ADJ]', trashAdj);
+        phrase = phrase.replace('[ITEM]', newTrash);
+        phrase = phrase.replace('[COUNT]', trashCountNum);
 
-        var sendText = "wowee";
-        var sorryRand = Math.floor(Math.random() * 100) + 1; //returns a random number from 1 to 100
-        if(sorryRand > 92) {
-            sendText = `Sorry, ${trashPerson}, but I couldn't resist. I ate your ${trashAdj} ${newTrash}. There have been ${trashCountNum} muffins given out on ${message.guild.name}.`
-        } else {
-            sendText = `${phrase} Kec has given out ${trashCountNum} muffins on ${message.guild.name}.`
-        }
-
-        if(newTrash == "muffin time") {
-            sendText = "https://tenor.com/view/muffin-time-the-muffin-song-muffin-clock-gif-15999617";
-        }
+        var sendText = `${phrase} Kec has given out ${trashCountNum} muffins on ${message.guild.name}.`;
 
         message.channel.send(sendText).then(function (botSentMessage) {
 
