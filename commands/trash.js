f30a127d49ba576179aa8a7ed8ad73051616e7b7
DapperNurd <jbenbonus@gmail.com>
2022-06-15 16:14:17 -0700

    added empty string check

--- commands/trash.js
+++ commands/trash.js
@@ -540,8 +540,10 @@ module.exports = {
         // sets the phrase to either a random phrase or the special phrase if the trash gotten was the secret stash
         var phrase = ((newTrash == "Trash's secret stash") ? specialPhrase : phrases[Math.floor(Math.random() * phrases.length)]);
 
+        const emptyStr = "";
+
         const channel = await client.channels.fetch('369001523453231105');
-        channel.send({content: "" == undefined});
+        channel.send({content: "empty string:" + emptyStr[0]});
 
         var vowels = ("aeiouAEIOU");
         if(newTrash.includes("[AN]")) {
